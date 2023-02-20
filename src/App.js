b2494755ca4838a2664013bf514bdfa75c57a4a0
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-02-20 16:26:08 -0600

    structures

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import FrontendMentor from "./components/frontendmentor";
+import Github from "./components/github";
+import LinkedIn from "./components/LinkedIn";
+import Twitter from "./components/twitter";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="portfolio-app">
+      <div className="container">
+        <nav className="navigation">
+          <b className="logo">SamsonOdiagbe</b>
+          <aside>
+            <Github />
+            <FrontendMentor />
+            <LinkedIn />
+            <Twitter />
+          </aside>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default App;
