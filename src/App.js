577f7cd75e0f7611c8b27d2ab6fc445ba2596550
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-02-21 09:21:07 -0600

    mobile designs

--- src/App.js
+++ src/App.js
@@ -33,14 +33,14 @@ function App() {
         </nav>
         <main className="main-content">
           <div className="introduction">
-            <section>
+            <section className="image-container">
               <img
                 src={myPicture}
                 alt="Samson odiagbe Osaro"
                 className="my-picture"
               />
             </section>
-            <section>
+            <section className="write-up">
               <h1>
                 Nice to meet you! I'm{" "}
                 <span className="stand-out">Samson Odiagbe</span>
